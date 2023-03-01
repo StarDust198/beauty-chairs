84e541303285428b9a5ea45ec76b66aa48acac38
Sergei Zhilinskij <roadtomars2030@gmail.com>
2023-03-01 19:19:04 +0400

    add CalcScroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import * as flsFunctions from './modules/functons.js';
 import { menuModal } from './modules/menu.js';
+// import { calcScroll } from './modules/calcScroll.js';
 
 flsFunctions.isWebP();
 menuModal();
